774f79301ccb7d00b21bb654965c9aa252f75782
s4nj1th <sanjith@tutamail.com>
2025-02-16 13:55:34 +0530

    reverted to old commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import Filler from './Filler';
 import './App.css'
 import FileUpload from './FileUpload';
-import Info from './Info';
 
 function App() {
 
   return (
     <>
-    <div className="app">
-      <Filler />
-      <FileUpload />
-    </div>
-    <div className="info">
-      <Info />
-    </div>
+    <Filler />
+    <FileUpload />
     </>
   )
 }
